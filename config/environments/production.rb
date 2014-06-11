0a382af2bbde3358c7166b2f61f1a4f2fa50c2dd
ParthivPatel-BTC <parthiv.patel@beaverlogic.com>
2014-06-11 16:36:07 +0530

    removed code from production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ AceThemeRails4Application::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-   config.assets.precompile += %w( font-awesome-ie7.min.css jquery-ui-1.10.3.custom.min.css font-awesome.css.scss.erb ace.min.css ace-ie.min.css )
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
